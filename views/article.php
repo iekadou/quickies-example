653a950b34064e1a29e916363a71e7ed3d30434a
Jonas Braun <jonas.braun@particulate.me>
2017-08-09 17:10:10 +0200

    better naming

--- views/article.php
+++ views/article.php
@@ -5,9 +5,9 @@ use Iekadou\Quickies\Utils;
 use Iekadou\Quickies\View;
 
 
-$id = (isset($_GET['id']) ? htmlspecialchars($_GET['id']) : false);
+$slug = (isset($_GET['slug']) ? htmlspecialchars($_GET['slug']) : false);
 
-$Article = _i(Article::_cn)->get_by(array(array("slug", "=", $id), array("public", "=", 1)));
+$Article = _i(Article::_cn)->get_by(array(array("slug", "=", $slug), array("public", "=", 1)));
 if (!$Article) {
     Utils::raise404();
     die();
@@ -15,4 +15,3 @@ if (!$Article) {
 $View = new View($Article->slug, $Article->name, 'article.html');
 $View->set_template_var('article', $Article);
 $View->render();
-
